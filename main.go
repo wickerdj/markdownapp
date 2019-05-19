73e4a469a8ae2d1cad7f92efc8e3b249dada9398
Wick <wickerdj@gmail.com>
2019-05-18 19:22:16 -0500

    added a different routing library

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
+	"github.com/julienschmidt/httprouter"
 	"gopkg.in/russross/blackfriday.v2"
 )
 
@@ -13,13 +15,28 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	r := httprouter.New()
+	r.NotFound = http.FileServer(http.Dir("public"))
+	r.GET("/markdown", GenerateMarkdown)
+
+	r.GET("/posts", PostsIndexHandler)
+	r.POST("/posts", PostsCreateHandler)
+
 	http.ListenAndServe(":"+port, nil)
 }
 
 // GenerateMarkdown creates the markdown
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	markdown := blackfriday.Run([]byte(r.FormValue("body")))
 	rw.Write(markdown)
+}
+
+// PostsIndexHandler prints a simple message
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts index")
+}
+
+// PostsCreateHandler prints a simple message
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts create")
 }
